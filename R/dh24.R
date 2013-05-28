aeb205c7235611639d978d4bb51ff9184f3f6739
jlthomps <jlthomps@usgs.gov>
2013-05-28 14:46:15 -0500

    added thresh input to example

--- R/dh24.R
+++ R/dh24.R
@@ -10,7 +10,7 @@
 #' @examples
 #' load_data<-paste(system.file(package="HITHATStats"),"/data/obs_data.csv",sep="")
 #' qfiletempf<-read.csv(load_data)
-#' dh24(qfiletempf)
+#' dh24(qfiletempf, 1158)
 dh24 <- function(qfiletempf, thresh) {
   lfcrit <- thresh
   noyears <- aggregate(qfiletempf$discharge, list(qfiletempf$wy_val), 
