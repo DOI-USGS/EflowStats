92571a94f9a50be4fa9bd52bf8fccccfb3debfbd
jlthomps <jlthomps@usgs.gov>
2013-08-06 16:12:53 -0500

    fixes, but still not final

--- R/tl3.R
+++ R/tl3.R
@@ -17,31 +17,28 @@
 #' tl3(qfiletempf, 1161.38)
 tl3 <- function(qfiletempf, thresh) {
   lfcrit <- thresh
-  qfiletempf$season <- ifelse(as.numeric(qfiletempf$month)==10,10,ifelse(as.numeric(qfiletempf$month)==11,10,ifelse(as.numeric(qfiletempf$month)==12,12,ifelse(as.numeric(qfiletempf$month)==1,12,ifelse(as.numeric(qfiletempf$month)==2,2,ifelse(as.numeric(qfiletempf$month)==3,2,ifelse(as.numeric(qfiletempf$month)==4,4,ifelse(as.numeric(qfiletempf$month)==5,4,ifelse(as.numeric(qfiletempf$month)==6,6,ifelse(as.numeric(qfiletempf$month)==7,6,ifelse(as.numeric(qfiletempf$month)==8,8,ifelse(as.numeric(qfiletempf$month)==9,8,99))))))))))))
-  nomonyears <- aggregate(qfiletempf$discharge, list(qfiletempf$wy_val,qfiletempf$season), 
+  nomonyears <- aggregate(qfiletempf$discharge, list(qfiletempf$wy_val,qfiletempf$month_val), 
                           FUN = median, na.rm=TRUE)
-  colnames(nomonyears) <- c("Year","season", "momax")
-  nomonyrs <- length(nomonyears$Year)
-  dur <- data.frame(Year = rep(0,nrow(nomonyears)), season = rep(0,nrow(nomonyears)), dur = rep(1,nrow(nomonyears)))
-  nlf <- rep(0,12)
-  tnlf <- 0
+  colnames(nomonyears) <- c("Year","month", "momax")
+  nomonyrs <- nrow(nomonyears)
+  dur <- data.frame(Year = rep(0,nrow(nomonyears)), month = rep(0,nrow(nomonyears)), dur = rep(1,nrow(nomonyears)))
   for (i in 1:nomonyrs) {
     monyears <- nomonyears[i,]
-    colnames(monyears) <- c("wy_val","season","momax")
-    subsetyr <- merge(qfiletempf,monyears,by = c("wy_val","season"))
+    colnames(monyears) <- c("wy_val","month_val","momax")
+    subsetyr <- merge(qfiletempf,monyears,by = c("wy_val","month_val"))
     flag <- 0
-    tnlf <- 0
+    nevents <- 0
     for (j in 1:nrow(subsetyr)) {
-      if (subsetyr$discharge[j]<lfcrit) {
+      if (subsetyr$discharge[j]<=lfcrit) {
         flag <- flag+1
-        tnlf <- ifelse(flag==1,tnlf+1,tnlf)
-        nlf[subsetyr$season] <- ifelse(flag==1,nlf[subsetyr$season]+1,nlf[subsetyr$season])
+        nevents <- ifelse(flag==1,nevents+1,nevents)
         dur$Year[i] <- subsetyr$wy_val[j]
-        dur$season[i] <- subsetyr$season[j]
-        dur$dur[i] <- tnlf
+        dur$month[i] <- subsetyr$month_val[j]
+        dur$dur[i] <- nevents
       } else {flag <- 0}
     }
   }
+  dur$season <- ifelse(as.numeric(dur$month)==10,10,ifelse(as.numeric(dur$month)==11,10,ifelse(as.numeric(dur$month)==12,12,ifelse(as.numeric(dur$month)==1,12,ifelse(as.numeric(dur$month)==2,2,ifelse(as.numeric(dur$month)==3,2,ifelse(as.numeric(dur$month)==4,4,ifelse(as.numeric(dur$month)==5,4,ifelse(as.numeric(dur$month)==6,6,ifelse(as.numeric(dur$month)==7,6,ifelse(as.numeric(dur$month)==8,8,ifelse(as.numeric(dur$month)==9,8,99))))))))))))
   dur <- dur[!dur$season==99,]
   num_season <- aggregate(dur$dur, list(dur$season), sum)
   tl3 <- max(num_season$x)/sum(num_season$x)
