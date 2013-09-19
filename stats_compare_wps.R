7e6697c78f3162450be90a9bf3f8398602031422
jlthomps <jlthomps@usgs.gov>
2013-09-19 10:08:01 -0500

    testing

--- stats_compare_wps.R
+++ stats_compare_wps.R
@@ -28,15 +28,48 @@ a<-t(getcap$scenario_sites)
 a2<-a
 al<-length(a)
 
+Flownum <- (length(grep("magStat",stats))*5)+(length(grep("flowStat",stats))*3)+(length(grep("durStat",stats))*3)+(length(grep("timStat",stats))*3)+(length(grep("rateStat",stats))*3)+(length(grep("otherStat",stats))*12)
+GoFnum <- (length(grep("GOF",stats))*50)+(length(grep("GOFMonth",stats))*96)
+Magnifnum <- (length(grep("magnifSeven",stats))*7)
 comment<-vector(length=al)
-ObsFlowStats <- matrix(nrow=al,ncol=29)
+ObsFlowStats <- matrix(nrow=al,ncol=Flownum)
 ModFlowStats <- matrix(nrow=nrow(ObsFlowStats),ncol=ncol(ObsFlowStats))
-magnifSevenObs <- matrix(nrow=nrow(ObsFlowStats),ncol=7)
-magnifSevenMod <- matrix(nrow=nrow(ObsFlowStats),ncol=7)
-GoFMetrics <- matrix(nrow=nrow(ObsFlowStats),ncol=146)
+FlowStats.PDiff <- matrix(nrow=nrow(ObsFlowStats),ncol=ncol(ObsFlowStats))
+magnifSevenObs <- matrix(nrow=nrow(ObsFlowStats),ncol=Magnifnum)
+magnifSevenMod <- matrix(nrow=nrow(ObsFlowStats),ncol=Magnifnum)
+magnifSeven.PDiff <- matrix(nrow=nrow(ObsFlowStats),ncol=Magnifnum)
+GoFMetrics <- matrix(nrow=nrow(ObsFlowStats),ncol=GoFnum)
 #MonAnnGoF <- matrix(nrow=nrow(ObsFlowStats),ncol=84)
 yv<-vector(length=al)
 ymaxv<-vector(length=al)
+namesMagnif <- c('lam1Obs','tau2Obs','tau3Obs','tau4Obs','ar1Obs','amplitudeObs','phaseObs','lam1Mod','tau2Mod','tau3Mod','tau4Mod','ar1Mod','amplitudeMod','phaseMod','lam1Diff','tau2Diff','tau3Diff','tau4Diff','ar1Diff','amplitudeDiff','phaseDiff')
+namesMagStat <- c('ma26Obs','ma41Obs','ml18Obs','ml20Obs','mh10Obs','ma26Mod','ma41Mod','ml18Mod','ml20Mod','mh10Mod','ma26Diff','ma41Diff','ml18Diff','ml20Diff','mh10Diff')
+namesFlowStat <- c('fl2Obs','fh6Obs','fh7Obs','fl2Mod','fh6Mod','fh7Mod','fl2Diff','fh6Diff','fh7Diff')
+namesDurStat <- c('dl6Obs','dh13Obs','dh16Obs','dl6Mod','dh13Mod','dh16DMod','dl6Diff','dh13Diff','dh16Diff')
+namesTimStat <- c('ta1Obs','tl1Obs','th1Obs','ta1Mod','tl1Mod','th1Mod','ta1Diff','tl1Diff','th1Diff')
+namesRateStat <- c('ra5Obs','ra7Obs','ra8Obs','ra5Mod','ra7Mod','ra8Mod','ra5Diff','ra7Diff','ra8Diff')
+namesOtherStat <- c('med_flowObs','cv_flowObs','cv_dailyObs','l7Q10Obs','l7Q2Obs','return_10Obs','flow_10Obs','flow_25Obs','flow_50Obs','flow_75Obs','flow_90Obs','flow_15Obs','med_flowMod','cv_flowMod','cv_dailyMod','l7Q10Mod','l7Q2Mod','return_10Mod','flow_10Mod','flow_25Mod','flow_50Mod','flow_75Mod','flow_90Mod','flow_15Mod','med_flowDiff','cv_flowDiff','cv_dailyDiff','l7Q10Diff','l7Q2Diff','return_10Diff','flow_10Diff','flow_25Diff','flow_50Diff','flow_75Diff','flow_90Diff','flow_15Diff')
+namesGoF <- c('nse','nselog','rmse','rmsne','rsr','pbias','pearson','spearman',
+              'nse_90','nse_75_90','nse_50_75','nse_25_50','nse_10_25','nse_10',
+              'rmse_90','rmse_75_90','rmse_50_75','rmse_25_50','rmse_10_25','rmse_10',
+              'rmsne_90','rmsne_75_90','rmsne_50_75','rmsne_25_50','rmsne_10_25','rmsne_10',
+              'rsr_90','rsr_75_90','rsr_50_75','rsr_25_50','rsr_10_25','rsr_10',
+              'pbias_90','pbias_75_90','pbias_50_75','pbias_25_50','pbias_10_25','pbias_10',           
+              'pearson_90','pearson_75_90','pearson_50_75','pearson_25_50','pearson_10_25','pearson_10',
+              'spearman_90','spearman_75_90','spearman_50_75','spearman_25_50','spearman_10_25','spearman_10')
+namesGoFMonth <- c('NSEbyMonthJan','NSELOGbyMonthJan','RMSEbyMonthJan','RMSNEbyMonthJan','RSRbyMonthJan','BiasbyMonthJan','PearsonbyMonthJan','SpearmanbyMonthJan',
+                   'NSEbyMonthFeb','NSELOGbyMonthFeb','RMSEbyMonthFeb','RMSNEbyMonthFeb','RSRbyMonthFeb','BiasbyMonthFeb','PearsonbyMonthFeb','SpearmanbyMonthFeb',
+                   'NSEbyMonthMar','NSELOGbyMonthMar','RMSEbyMonthMar','RMSNEbyMonthMar','RSRbyMonthMar','BiasbyMonthMar','PearsonbyMonthMar','SpearmanbyMonthMar',
+                   'NSEbyMonthApr','NSELOGbyMonthApr','RMSEbyMonthApr','RMSNEbyMonthApr','RSRbyMonthApr','BiasbyMonthApr','PearsonbyMonthApr','SpearmanbyMonthApr',
+                   'NSEbyMonthMay','NSELOGbyMonthMay','RMSEbyMonthMay','RMSNEbyMonthMay','RSRbyMonthMay','BiasbyMonthMay','PearsonbyMonthMay','SpearmanbyMonthMay',
+                   'NSEbyMonthJun','NSELOGbyMonthJun','RMSEbyMonthJun','RMSNEbyMonthJun','RSRbyMonthJun','BiasbyMonthJun','PearsonbyMonthJun','SpearmanbyMonthJun',
+                   'NSEbyMonthJul','NSELOGbyMonthJul','RMSEbyMonthJul','RMSNEbyMonthJul','RSRbyMonthJul','BiasbyMonthJul','PearsonbyMonthJul','SpearmanbyMonthJul',
+                   'NSEbyMonthAug','NSELOGbyMonthAug','RMSEbyMonthAug','RMSNEbyMonthAug','RSRbyMonthAug','BiasbyMonthAug','PearsonbyMonthAug','SpearmanbyMonthAug',
+                   'NSEbyMonthSep','NSELOGbyMonthSep','RMSEbyMonthSep','RMSNEbyMonthSep','RSRbyMonthSep','BiasbyMonthSep','PearsonbyMonthSep','SpearmanbyMonthSep',
+                   'NSEbyMonthOct','NSELOGbyMonthOct','RMSEbyMonthOct','RMSNEbyMonthOct','RSRbyMonthOct','BiasbyMonthOct','PearsonbyMonthOct','SpearmanbyMonthOct',
+                   'NSEbyMonthNov','NSELOGbyMonthNov','RMSEbyMonthNov','RMSNEbyMonthNov','RSRbyMonthNov','BiasbyMonthNov','PearsonbyMonthNov','SpearmanbyMonthNov',
+                   'NSEbyMonthDec','NSELOGbyMonthDec','RMSEbyMonthDec','RMSNEbyMonthDec','RSRbyMonthDec','BiasbyMonthDec','PearsonbyMonthDec','SpearmanbyMonthDec')
+namesFull <- c('site_no','min_date','max_date')
 
 for (i in 1:length(a2)){
   modsites<-a2[i]
@@ -47,7 +80,7 @@ for (i in 1:length(a2)){
     enddate<-max(x_mod$date)
     interval<-''
     latest<-''
-    sites<-a[i]
+    sites=a[i]
     url2<-paste(sos_url_temp,sites,'&startDT=',startdate,'&endDT=',enddate,'&statCd=',offering_temp,'&parameterCd=',property_temp,sep='')
     x_obs <- getXMLWML1.1Data(url2)
     
@@ -87,37 +120,29 @@ for (i in 1:length(a2)){
             yv[i]<-as.character(min(obs_data$date))
             ymaxv[i]<-as.character(max(obs_data$date))
             cat(paste("dates calculated for site",sites,"\n",sep=" "))
-            #x_modz<-mod_data$discharge
-            #x_obsz<-obs_data$discharge
-            #dates<-as.Date(obs_data$date)
-            #file<-paste("graph",toString(sites),".png",sep="")
-            #png(file)
-            #ggof(x_modz,x_obsz,na.rm=FALSE,dates,main=modsites)
-            #dev.copy(png,file)
-            #dev.off()
-            #file<-paste("monthly_mean_ts_obs",toString(sites),".txt",sep="")
-            #monthly_mean<-monthly.mean.ts(obs_data)
-            #write.table(monthly_mean,file=file,col.names=TRUE, row.names=FALSE, quote=FALSE, sep="\t")
-            #file<-paste("monthly_mean_ts_mod",toString(sites),".txt",sep="")
-            #monthly_mean<-monthly.mean.ts(mod_data)
-            #write.table(monthly_mean,file=file,col.names=TRUE, row.names=FALSE, quote=FALSE, sep="\t")
             
             obs_data <- obs_data[,c('wy_val','date','discharge','month_val','year_val','day_val','jul_val')]
             mod_data <- mod_data[,c('wy_val','date','discharge','month_val','year_val','day_val','jul_val')]
             obs_count <- nrow(obs_data)
             mod_count <- nrow(mod_data)
             cat(paste("dfs created for site",sites,obs_count,mod_count,"\n",sep=" "))
-            ObsFlowStats[i,] <- FlowStats(obs_data,drain_area)
-            cat(paste("Obs flow stats calculated for site",sites,"\n",sep=" "))
-            ModFlowStats[i,] <- FlowStats(mod_data,drain_area)
-            cat(paste("Mod flow stats calculated for site",sites,"\n",sep=" "))
-            magnifSevenObs[i,] <- magnifSeven(obs_data)
-            cat(paste("Obs mag7 stats calculated for site",sites,"\n",sep=" "))
-            magnifSevenMod[i,] <- magnifSeven(mod_data)
-            cat(paste("Mod mag7 stats calculated for site",sites,"\n",sep=" "))
+            if (Flownum>0) {
+              ObsFlowStats[i,] <- FlowStats(obs_data,drain_area,stats)
+              cat(paste("Obs flow stats calculated for site",sites,"\n",sep=" "))
+              ModFlowStats[i,] <- FlowStats(mod_data,drain_area,stats)
+              cat(paste("Mod flow stats calculated for site",sites,"\n",sep=" "))
+            }
+            if (Magnifnum>0) {
+              magnifSevenObs[i,] <- magnifSeven(obs_data)
+              cat(paste("Obs mag7 stats calculated for site",sites,"\n",sep=" "))
+              magnifSevenMod[i,] <- magnifSeven(mod_data)
+              cat(paste("Mod mag7 stats calculated for site",sites,"\n",sep=" "))
+            }
             comment <- ""
-            GoFMetrics[i,] <- SiteGoF(obs_data,mod_data)
-            cat(paste("stats calculated for site",sites,"\n",sep=" "))
+            if (GOFnum>0) {
+              GoFMetrics[i,] <- SiteGoF(obs_data,mod_data,stats)
+              cat(paste("stats calculated for site",sites,"\n",sep=" "))
+            }
             #      MonAnnGoF[i,] <- MonthlyAnnualGoF(obs_data,mod_data)
           }
         }}
@@ -128,50 +153,56 @@ for (i in 1:length(a2)){
     comment[i]<-"No modeled data for site"
   } 
 }
-
-FlowStats.PDiff <- (ModFlowStats-ObsFlowStats)/ObsFlowStats
-magnifSeven.PDiff <- (magnifSevenMod-magnifSevenObs)/magnifSevenObs
+if (Flownum>0) {
+  FlowStats.PDiff <- (ModFlowStats-ObsFlowStats)/ObsFlowStats
+}
+if (Magnifnum>0) {
+  magnifSeven.PDiff <- (magnifSevenMod-magnifSevenObs)/magnifSevenObs
+}
 #FlowStats.GoF <- RegionalGoF(ObsFlowStats,ModFlowStats)
 cat("diffs calculated \n")
-statsout<-data.frame(t(a),yv,ymaxv,GoFMetrics,magnifSevenObs,ObsFlowStats,magnifSevenMod,ModFlowStats,magnifSeven.PDiff,FlowStats.PDiff,comment,stringsAsFactors=FALSE)
-colnames(statsout)<-c('site_no','min_date','max_date','nse','nselog','rmse','rmsne','rsr','pbias','pearson','spearman',
-                      'nse_90','nse_75_90','nse_50_75','nse_25_50','nse_10_25','nse_10',
-                      'rmse_90','rmse_75_90','rmse_50_75','rmse_25_50','rmse_10_25','rmse_10',
-                      'rmsne_90','rmsne_75_90','rmsne_50_75','rmsne_25_50','rmsne_10_25','rmsne_10',
-                      'rsr_90','rsr_75_90','rsr_50_75','rsr_25_50','rsr_10_25','rsr_10',
-                      'pbias_90','pbias_75_90','pbias_50_75','pbias_25_50','pbias_10_25','pbias_10',           
-                      'pearson_90','pearson_75_90','pearson_50_75','pearson_25_50','pearson_10_25','pearson_10',
-                      'spearman_90','spearman_75_90','spearman_50_75','spearman_25_50','spearman_10_25','spearman_10',
-                      'NSEbyMonthJan','NSELOGbyMonthJan','RMSEbyMonthJan','RMSNEbyMonthJan','RSRbyMonthJan','BiasbyMonthJan','PearsonbyMonthJan','SpearmanbyMonthJan',
-                      'NSEbyMonthFeb','NSELOGbyMonthFeb','RMSEbyMonthFeb','RMSNEbyMonthFeb','RSRbyMonthFeb','BiasbyMonthFeb','PearsonbyMonthFeb','SpearmanbyMonthFeb',
-                      'NSEbyMonthMar','NSELOGbyMonthMar','RMSEbyMonthMar','RMSNEbyMonthMar','RSRbyMonthMar','BiasbyMonthMar','PearsonbyMonthMar','SpearmanbyMonthMar',
-                      'NSEbyMonthApr','NSELOGbyMonthApr','RMSEbyMonthApr','RMSNEbyMonthApr','RSRbyMonthApr','BiasbyMonthApr','PearsonbyMonthApr','SpearmanbyMonthApr',
-                      'NSEbyMonthMay','NSELOGbyMonthMay','RMSEbyMonthMay','RMSNEbyMonthMay','RSRbyMonthMay','BiasbyMonthMay','PearsonbyMonthMay','SpearmanbyMonthMay',
-                      'NSEbyMonthJun','NSELOGbyMonthJun','RMSEbyMonthJun','RMSNEbyMonthJun','RSRbyMonthJun','BiasbyMonthJun','PearsonbyMonthJun','SpearmanbyMonthJun',
-                      'NSEbyMonthJul','NSELOGbyMonthJul','RMSEbyMonthJul','RMSNEbyMonthJul','RSRbyMonthJul','BiasbyMonthJul','PearsonbyMonthJul','SpearmanbyMonthJul',
-                      'NSEbyMonthAug','NSELOGbyMonthAug','RMSEbyMonthAug','RMSNEbyMonthAug','RSRbyMonthAug','BiasbyMonthAug','PearsonbyMonthAug','SpearmanbyMonthAug',
-                      'NSEbyMonthSep','NSELOGbyMonthSep','RMSEbyMonthSep','RMSNEbyMonthSep','RSRbyMonthSep','BiasbyMonthSep','PearsonbyMonthSep','SpearmanbyMonthSep',
-                      'NSEbyMonthOct','NSELOGbyMonthOct','RMSEbyMonthOct','RMSNEbyMonthOct','RSRbyMonthOct','BiasbyMonthOct','PearsonbyMonthOct','SpearmanbyMonthOct',
-                      'NSEbyMonthNov','NSELOGbyMonthNov','RMSEbyMonthNov','RMSNEbyMonthNov','RSRbyMonthNov','BiasbyMonthNov','PearsonbyMonthNov','SpearmanbyMonthNov',
-                      'NSEbyMonthDec','NSELOGbyMonthDec','RMSEbyMonthDec','RMSNEbyMonthDec','RSRbyMonthDec','BiasbyMonthDec','PearsonbyMonthDec','SpearmanbyMonthDec',
-                      'lam1Obs','tau2Obs','tau3Obs','tau4Obs','ar1Obs','amplitudeObs','phaseObs',
-                      'med_flowObs','cv_flowObs','cv_dailyObs','ma26Obs','ma41Obs','ml18Obs','ml20Obs',
-                      'mh10Obs','fl2Obs','fh6Obs','fh7Obs','dl6Obs','dh13Obs','dh16Obs','ta1Obs','tl1Obs','th1Obs','ra5Obs','ra7Obs','ra8Obs',
-                      'l7Q10Obs','l7Q2Obs','return_10Obs','flow_10Obs','flow_25Obs','flow_50Obs','flow_75Obs','flow_90Obs','flow_15Obs',
-                      'lam1Mod','tau2Mod','tau3Mod','tau4Mod','ar1Mod','amplitudeMod','phaseMod',
-                      'med_flowMod','cv_flowMod','cv_dailyMod','ma26Mod','ma41Mod','ml18Mod','ml20Mod',
-                      'mh10Mod','fl2Mod','fh6Mod','fh7Mod','dl6Mod','dh13Mod','dh16Mod','ta1Mod','tl1Mod','th1Mod','ra5Mod','ra7Mod','ra8Mod',
-                      'l7Q10Mod','l7Q2Mod','return_10Mod','flow_10Mod','flow_25Mod','flow_50Mod','flow_75Mod','flow_90Mod','flow_15Mod',
-                      'lam1Diff','tau2Diff','tau3Diff','tau4Diff','ar1Diff','amplitudeDiff','phaseDiff',
-                      'med_flowDiff','cv_flowDiff','cv_dailyDiff','ma26Diff','ma41Diff','ml18Diff','ml20Diff',
-                      'mh10Diff','fl2Diff','fh6Diff','fh7Diff','dl6Diff','dh13Diff','dh16Diff','ta1Diff','tl1Diff','th1Diff','ra5Diff','ra7Diff','ra8Diff',
-                      'l7Q10Diff','l7Q2Diff','return_10Diff','flow_10Diff','flow_25Diff','flow_50Diff','flow_75Diff','flow_90Diff','flow_15Diff','comment')  
+statsout<-data.frame(t(a),yv,ymaxv,GoFMetrics,magnifSevenObs,magnifSevenMod,magnifSeven.PDiff,ObsFlowStats,ModFlowStats,FlowStats.PDiff,comment,stringsAsFactors=FALSE)
+
+if (length(grep("GOF",stats))>0) {
+  namesFull <- c(namesFull,namesGoF)
+}
+if (length(grep("GOFMonth",stats))>0) {
+  namesFull <- c(namesFull,namesGoFMonth)
+}
+if (Magnifnum>0) {
+  namesFull <- c(namesFull,namesMagnif)
+}
+if (length(grep("otherStat",stats))>0) {
+  namesFull <- c(namesFull,namesOtherStat)
+}
+if (length(grep("magStat",stats))>0) {
+  namesFull <- c(namesFull,namesMagStat)
+}
+if (length(grep("flowStat",stats))>0) {
+  namesFull <- c(namesFull,namesFlowStat)
+}
+if (length(grep("durStat",stats))>0) {
+  namesFull <- c(namesFull,namesDurStat)
+}
+if (length(grep("timStat",stats))>0) {
+  namesFull <- c(namesFull,namesTimStat)
+}
+if (length(grep("rateStat",stats))>0) {
+  namesFull <- c(namesFull,namesRateStat)
+}
+namesFull <- c(namesFull,'comment')
+
+colnames(statsout)<-namesFull
 cat("statsout created and named \n")
-output="output.txt"
+output="output.zip"
 if (i==length(a2)) {
   write.table(statsout,file="output.txt",col.names=TRUE, row.names=FALSE, quote=FALSE, sep="\t")
+  system("rm output.zip")
+  #system("zip -r output graph*png")
+  #system("zip -r output monthly*txt")
+  system("zip -r output output*")
 } else { 
-  output="output.txt" 
+  output="output.zip" 
   message<-"One or more web service calls resulted in failure. Please try again."
   write.table(message,file="output.txt",col.names=FALSE,row.names=FALSE,quote=FALSE)
 }
