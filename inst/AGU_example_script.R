cf2419e07f2c97a8555e6fe8fc9957dbe32a9cad
jlthomps <jlthomps@usgs.gov>
2014-05-21 12:53:43 -0500

    added new observedFlowStatsMulti function example

--- inst/AGU_example_script.R
+++ inst/AGU_example_script.R
@@ -18,6 +18,11 @@ daily_data<-dailyData
 stats="magnifSeven,magStat,flowStat,durStat,timStat,rateStat,otherStat"
 statsout <- ObservedStatsOther(daily_data,drain_area,site_id,stats)
 
+# calculate stats for multiple sites in a local data directory
+dataPath="/Users/jlthomps/Documents/R/JData/modeled/"
+stats="magnifSeven,magStat,flowStat,durStat,timStat,rateStat,otherStat"
+statsout <- ObservedStatsOtherMulti(dataPath,stats)
+
 # plot monthly means for a daily discharge timeseries
 qfiletempf<-sampleData
 meanmonts<-monthlyMeanTs(qfiletempf)
