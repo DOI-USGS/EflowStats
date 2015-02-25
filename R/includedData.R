2fec0693ec6f1fbd1212a76b8fbf3246b0ebd1a8
unknown <ldecicco@usgs.gov>
2015-02-25 10:48:41 -0600

    Added disclaimer.

--- R/includedData.R
+++ R/includedData.R
@@ -30,3 +30,7 @@ NULL
 #' @author Jessica Thompson \email{jlthomps@@usgs.gov}
 #' @keywords data
 NULL
+
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("Although this software program has been used by the U.S. Geological Survey (USGS), no warranty, expressed or implied, is made by the USGS or the U.S. Government as to the accuracy and functioning of the program and related program material nor shall the fact of distribution constitute any such warranty, and no responsibility is assumed by the USGS in connection therewith.")
+}
