361d3901a39412d14061314e80b03669f49a4b2f
David Blodgett <dblodgett@usgs.gov>
2017-04-07 13:27:01 -0500

    theoretically, this will now work with the NWC portal, needs to handle multiple sites to be like it was before though.

--- demo/nwis_demo.R
+++ demo/nwis_demo.R
@@ -5,24 +5,54 @@
 # wps.in: stats, string, Statistic Groups, A list of statistic groups chosen from magnifSeven magStat flowStat durStat timStat rateStat;
 
 library(EflowStats)
-library(NWCCompare)
+library(dataRetrieval)
+
 ## Inputs: uncomment for non Rserve execuation. ##
-# sites <- '02177000,02178400'
-# startdate <- "2008-10-01"
-# enddate <- "2013-09-29"
-#stats<-"rateStat,magnifSeven,magStat,flowStat,durStat,timStat"
+sites <- '02177000' # only works with one for now.
+startdate <- "2008-10-01"
+enddate <- "2013-09-30"
+stats<-"rateStat,magnifSeven,magStat,flowStat,durStat,timStat"
 ## end inputs ##
 
-nwisDvUrl <- "https://waterservices.usgs.gov/nwis/dv/?format=waterml,1.1&sites="
-offering <- "00003"
-property <- "00060"
-drainage_url <- "https://waterservices.usgs.gov/nwis/site/?siteOutput=Expanded&site="
+# Need to get the stats list created from: "magAverage", "magLow", "magHigh", "frequencyLow", "frequencyHigh", "durationLow", "durationHigh", "timingAverage", "timingLow", "timingHigh", "rateChange"
+stats <- "all" # for now
 
 sites<-read.csv(header=F,colClasses=c("character"),text=sites)
-x_urls<-paste(nwisDvUrl, sites, "&startDT=", startdate, "&endDT=", enddate, "&statCd=", offering, "&parameterCd=", property, sep = "")
-d_urls<-paste(drainage_url, sites, sep = "")
-statsout <- calculateStatsGroups(stats, sites, startdate, enddate, getXMLWML1.1Data, x_urls, getDrainageArea, sites)
+
+dailyQ <- dataRetrieval::readNWISdv(siteNumber = sites[[1]],
+                                    parameterCd = "00060",
+                                    startDate = startdate,
+                                    endDate = enddate)
+dailyQClean <- dataCheck(dailyQ[c("Date","X_00060_00003")],yearType="water")
+
+siteInfo <- readNWISsite(siteNumber = "04085427")
+drainageArea <- siteInfo$drain_area_va
+
+peakFlows <- readNWISpeak(siteNumber = sites[[1]],
+                          startDate = startdate,
+                          endDate = enddate)
+
+floodThresh <- peakThreshold(dailyQClean[c("date","discharge")],
+                             peakFlows[c("peak_dt","peak_va")])
+
+statsTemp <- hitStats(dailyQClean,
+                        drainArea=drainageArea,
+                        floodThreshold=floodThresh)
+
+statsTemp <- rbind(magnifSeven(dailyQClean,yearType="water",digits=3), statsTemp)
+statsTemp[1:7,1] <- c("lam1Obs","tau2Obs","tau3Obs","tau4Obs","ar1Obs","amplitudeObs","phaseObs")
+
+statsout <- data.frame(matrix(NA, nrow=length(sites), ncol = (3 + nrow(statsTemp) + 1)))
+names(statsout) <- c("site_no", "min_date", "max_date", statsTemp[,1], "comment")
+
+statsout[1,1] <- sites[[1]]
+statsout[1,2] <- startdate # make sure this is the same as came back from the data?
+statsout[1,3] <- enddate # make sure this is the same as came back from the data?
+statsout[1,4:(length(statsout)-1)] <- statsTemp[,2]
+statsout[1,length(statsout)] <- ""
+
 output = "output.txt"
+
 write.table(statsout, file = output, col.names = TRUE, row.names = FALSE, quote = FALSE, sep = "\t")
 
 # wps.out: output, text, Output File, A text file containing the table of statistics as well as monthly stats and graphs for each site;
