9a826a948e40f457937e3c2fffaf2d6972b5f7aa
David Blodgett <dblodgett@usgs.gov>
2017-05-05 09:36:10 -0500

    A bit of cleanup of sampleData

--- tests/testthat/tests_durationHigh.R
+++ tests/testthat/tests_durationHigh.R
@@ -2,7 +2,6 @@ context("duration high")
 
 test_that("duration high", {
         ###load the data upfront#
-        load("data/sampleData.rda")
         load("data/durationHighOut.rda")
         x<-sampleData[c("date","discharge")]
         durationHighOutTest <- durationHigh(x=x,yearType="water")
