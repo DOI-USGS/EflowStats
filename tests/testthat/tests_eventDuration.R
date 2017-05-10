204600a8112e572644337db724aa8caa38e9ff91
David Blodgett <dblodgett@usgs.gov>
2017-05-10 14:08:26 -0500

    test coverage FTW!

--- tests/testthat/tests_eventDuration.R
+++ tests/testthat/tests_eventDuration.R
@@ -9,3 +9,13 @@ test_that("event duration", {
         
         expect_equal(eventDurationOutTest,eventDurationOut)
 })
+
+test_that("event duration trim", {
+        
+        x <- readRDS("data/sample_nwis_data.rds")$discharge
+        x <- x[100:915] # gets an event at the start and end.
+        threshold<-median(x, na.rm=TRUE)
+        eventDurationOutTest <- eventDuration(x, threshold, trim = TRUE, aggType = "min")
+        eventDurationOut <- readRDS("data/test_eventDuration_trim.rds")
+        expect_equal(eventDurationOutTest,eventDurationOut)
+})
