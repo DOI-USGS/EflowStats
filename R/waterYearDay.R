4597a2c31f8cce05ff8b1688c6224dcef0eda2df
David Blodgett <dblodgett@usgs.gov>
2017-05-16 09:45:12 -0500

    Commenting to clarify logic and adjusting variable names to be more expressive.

--- R/waterYearDay.R
+++ R/waterYearDay.R
@@ -12,14 +12,22 @@
 
 waterYearDay <- function(x) {
         
-        jday = lubridate::yday(x)
-        yrs_leap = lubridate::leap_year(x)
+        year_day <- lubridate::yday(x)
+        yrs_leap <- lubridate::leap_year(x)
+        
+        # October 1st (day 1 of water year) is day 274 (or 275 in leap year) of calendar year.
+        # (274 + years_leap) == 275 for dates in leap year and 274 for dates not in leap year.
+        # This provides a boolean selector for days before (false) / after (true) Oct 1.
+        after_waterday <- year_day >= (274 + yrs_leap)
 
-        gt_lp275 = jday >= (274 + yrs_leap)
+        # 273 (or 274 in leap year) days from January 1 to October 1
+        # This gives us 1 for October 1st and corrects everything till December 31st.
+        year_day[after_waterday] <- year_day[after_waterday] - (273 + yrs_leap[after_waterday])
+        
+        # 92 days from October 1 to January 1
+        # This gives us 93 for January 1 and corrects everything till September 29th.
+        year_day[!after_waterday] <- year_day[!after_waterday] + 92
 
-        jday[gt_lp275] = jday[gt_lp275] - (273 + yrs_leap[gt_lp275])
-        jday[!gt_lp275] = jday[!gt_lp275] + 92
-
-        return(jday)
+        return(year_day)
         
 }
