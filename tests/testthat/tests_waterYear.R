6f08f2d026141ece03b797ccd07cdc5d7458d46a
David Blodgett <dblodgett@usgs.gov>
2017-05-19 14:54:09 -0500

    MOAR TESTS

---
+++ tests/testthat/tests_waterYear.R
@@ -0,0 +1,13 @@
+context("waterYear")
+
+test_that("waterYear Works", {
+        expect_equal(waterYear(as.Date("2012-10-01")), 
+                     2013)
+        
+        expect_equal(waterYear(as.Date("2012-09-01")), 
+                     2012)
+        
+        expect_equal(waterYear(as.Date("2012-10-01"), numeric = FALSE), 
+                     factor("2013", ordered = T))
+})
+        
