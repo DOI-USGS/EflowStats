20caa442aeaf85585149880051febccf93e4dced
David Blodgett <dblodgett@usgs.gov>
2017-05-20 09:19:20 -0500

    tests pass locally but not on travis trying this

--- tests/testthat/tests_hitAllStats.R
+++ tests/testthat/tests_hitAllStats.R
@@ -9,8 +9,19 @@ test_that("HIT stats", {
         
         expect_equal(hitOutTest,hitOut)
         
+})
+
+test_that("HIT stats med", {
+        
+        x <- sampleData[c("date","discharge")]
+        
         hitOutTest <- hitAllStats(x=x,yearType="water",stats="all",pref="median",drainArea=50)
         hitOut <- readRDS("data/tests_hitAllStats_med.rds")
         
-        expect_equal(hitOutTest,hitOut)
+        for(stat in 1:nrow(hitOut)) {
+                calc <- setNames(hitOutTest[stat,],c("name", hitOutTest[stat,1]))
+                check <- setNames(hitOut[stat,],c("name", hitOut[stat,1]))
+                expect_equal(calc,check)
+        }
+        
 })
