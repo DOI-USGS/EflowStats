7f1e365f114d8f568a9ade12dbfda4d0816c11d7
David Blodgett <dblodgett@usgs.gov>
2017-05-20 10:17:38 -0500

    new test works now. duh.

--- tests/testthat/tests_hitAllStats.R
+++ tests/testthat/tests_hitAllStats.R
@@ -2,26 +2,22 @@ context("HIT stats")
 
 test_that("HIT stats", {
 
-        x <- sampleData[c("date","discharge")]
-        
-        hitOutTest <- hitAllStats(x=x,yearType="water",stats="all",pref="mean",drainArea=50)
-        hitOut <- readRDS("data/tests_hitAllStats.rds")
-        
-        expect_equal(hitOutTest,hitOut)
+        expect_equal_to_reference(hitAllStats(x=sampleData[c("date","discharge")],
+                                              yearType="water",
+                                              stats="all",
+                                              pref="mean",
+                                              drainArea=50),
+                                  "data/tests_hitAllStats.rds")
         
 })
 
 test_that("HIT stats med", {
         
-        x <- sampleData[c("date","discharge")]
-        
-        hitOutTest <- hitAllStats(x=x,yearType="water",stats="all",pref="median",drainArea=50)
-        hitOut <- readRDS("data/tests_hitAllStats_med.rds")
-        
-        for(stat in 1:nrow(hitOut)) {
-                calc <- setNames(hitOutTest[stat,],c("name", hitOutTest[stat,1]))
-                check <- setNames(hitOut[stat,],c("name", hitOut[stat,1]))
-                expect_equal(calc,check)
-        }
+        expect_equal_to_reference(hitAllStats(x=sampleData[c("date","discharge")],
+                                              yearType="water",
+                                              stats="all",
+                                              pref="median",
+                                              drainArea=50),
+                                  "data/tests_hitAllStats_med.rds")
         
 })
