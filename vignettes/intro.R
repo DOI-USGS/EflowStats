5a6b516fd662d8f3c270fbda1c17caf5653ec5ed
David Blodgett <dblodgett@usgs.gov>
2017-05-22 13:56:18 -0500

    Cleaning up the vignette. Starting to think about #126.

--- vignettes/intro.R
+++ vignettes/intro.R
@@ -4,12 +4,9 @@
 #  
 #  #Get some data
 #  dailyQ <- readNWISdv(siteNumber = "04085427",
-#  parameterCd = "00060",
-#  startDate = "2000-10-01",
-#  endDate = "2012-9-30")
-#  
-#  #Check data for completeness
-#  dailyQClean <- dataCheck(dailyQ[c("Date","X_00060_00003")],yearType="water")
+#                       parameterCd = "00060",
+#                       startDate = "2000-10-01",
+#                       endDate = "2012-9-30")
 #  
 #  #Get drainage area
 #  siteInfo <- readNWISsite(siteNumber = "04085427")
@@ -17,19 +14,25 @@
 #  
 #  #Get peak flows
 #  peakFlows <- readNWISpeak(siteNumber = "04085427",
-#  startDate = "2000-10-01",
-#  endDate = "2012-9-30")
+#                            startDate = "2000-10-01",
+#                            endDate = "2012-9-30")
+#  
+
+## ----message=FALSE, echo=TRUE, eval=FALSE--------------------------------
+#  
+#  #Check data for completeness
+#  dailyQClean <- dataCheck(dailyQ[c("Date", "X_00060_00003")], yearType="water")
 #  
 #  #Get flood recurence threshold
-#  floodThresh <- peakThreshold(dailyQClean[c("date","discharge")],
-#  peakFlows[c("peak_dt","peak_va")])
+#  floodThresh <- peakThreshold(dailyQClean[c("date", "discharge")],
+#                               peakFlows[c("peak_dt", "peak_va")])
 #  
 #  #Calculate all hit stats
 #  hitAllStatsOut <- hitAllStats(dailyQClean,
-#  drainArea=drainageArea,
-#  floodThreshold=floodThresh)
+#                                drainArea=drainageArea,
+#                                floodThreshold=floodThresh)
 #  
 #  #Calculate mag7 stats
-#  magnifStatsOut <- magnifSeven(dailyQClean,yearType="water",digits=3)
+#  magnifStatsOut <- magnifSeven(dailyQClean, yearType="water", digits=3)
 #  
 
