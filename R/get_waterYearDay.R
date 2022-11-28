bac728b5250ba138d2db2e4028e9f65edb141449
David Blodgett <dblodgett@usgs.gov>
2022-11-27 21:12:55 -0600

    testing and a littly cleanup

--- R/get_waterYearDay.R
+++ R/get_waterYearDay.R
@@ -10,12 +10,6 @@
 #' x <- seq(from=as.Date("2010-01-01"),to=as.Date("2016-01-01"),by="1 days")
 #' year_day <- get_waterYearDay(x, 2L)
 
-#write.csv(year_day,"year_day_2.csv",row.names=FALSE)
-#year_day_old <- get_waterYearDay_old(x)
-#write.csv(year_day_old,"year_day_old.csv",row.names=FALSE)
-#write.csv(x,"dates.csv",row.names=FALSE)
-
-
 get_waterYearDay <- function(x, wyMonth=10L) {
         
   doy <- lubridate::yday(x)  # day of year series
@@ -33,26 +27,4 @@ get_waterYearDay <- function(x, wyMonth=10L) {
   
   return(year_day)
         
-}
-
-get_waterYearDay_old <- function(x) {
-  
-  year_day <- lubridate::yday(x)
-  yrs_leap <- lubridate::leap_year(x)
-  
-  # October 1st (day 1 of water year) is day 274 (or 275 in leap year) of calendar year.
-  # (274 + years_leap) == 275 for dates in leap year and 274 for dates not in leap year.
-  # This provides a boolean selector for days before (false) / after (true) Oct 1.
-  after_waterday <- year_day >= (274 + yrs_leap)
-  
-  # 273 (or 274 in leap year) days from January 1 to October 1
-  # This gives us 1 for October 1st and corrects everything till December 31st.
-  year_day[after_waterday] <- year_day[after_waterday] - (273 + yrs_leap[after_waterday])
-  
-  # 92 days from October 1 to January 1
-  # This gives us 93 for January 1 and corrects everything till September 29th.
-  year_day[!after_waterday] <- year_day[!after_waterday] + 92
-  
-  return(year_day)
-  
 }
---
+++ tests/testthat/tests_cut_dataToWaterYear.R
@@ -0,0 +1,20 @@
+test_that("cut water year", {
+  x <- sampleData[, c('date', 'discharge')]
+  cut <- cut_dataToWaterYear(x, 8)
+  
+  expect_equal(cut$date[1], structure(15187, class = "Date"))  
+  
+  expect_equal(cut$year_val[1], 2012)
+  
+  expect_true(as.Date("2012-02-29") %in% cut$date)
+  
+  expect_equal(nrow(cut), 366)
+  
+  cut <- cut_dataToWaterYear(x, 11)
+  
+  expect_equal(cut$date[1], structure(14914, class = "Date"))  
+  
+  expect_equal(cut$year_val[1], 2011)
+  
+  expect_equal(nrow(cut), 365)
+})
