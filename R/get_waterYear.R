1522f1624c6ab7898d732e1ec06aa8152a489e2a
jenskiesel <jenskiesel@gmail.com>
2023-09-06 09:04:50 +0200

    Corrected the calculation of the water year to a more intuitive convention: the water year value should be the calendar year with the most dates in the water year. Therefore, a cutoff is assigned at wyMonth=7. See this issue for further information: https://github.com/DOI-USGS/EflowStats/issues/196

--- R/get_waterYear.R
+++ R/get_waterYear.R
@@ -10,50 +10,35 @@
 #' get_waterYear(sampleData$date)
 #' 
 get_waterYear <- function(x, wyMonth=10L, numeric=TRUE) {
-        ## Coding history:
-        ##    2005Jul14 DLLorenz Initial dated verion
-        ##    2010Feb17 DLLorenz Added option to return numerics
-        ##    2011Jun07 DLLorenz Conversion to R
-        ##    2012Aug11 DLLorenz Integer fixes
-        ##    2013Feb15 DLLorenz Prep for gitHub
-        ##    2016Aug12 Joe Mills copied this from smwrBase
-        ##    2023Aug04 Jens Kiesel added different calculation if wyMonth < 3
-        ##              (see explanation below)
-        x <- as.POSIXlt(x)
-        yr <- x$year + 1900L
-        mn <- x$mon + 1L  # x$mon starts at 0 for Jan!
-        if (wyMonth < 3){
-          # Jan must be associated to the previous year if wyMonth < 3 
-          # because of leap years. If an assigned water year includes the 
-          # 29th of Feb, it must be associated to a leap year:
-          # 
-          # If the water year starts in January (equal to the calendar year):
-          # yr for all months must be equal to the current yr.
-          # function "yr<-yr-ifelse(mn<wyMonth,1L,0L)" is then:
-          # yr <- yr - 0 (ifelse condition is false in all cases)
-          # 
-          # If the water year starts in February:
-          # yr for Jan must be the previous year and yr for all other
-          # months must be the current year
-          # function "yr<-yr-ifelse(mn<wyMonth,1L,0L)" for mn=1 < wyMonth=2:
-          # yr <- yr - 1 (ifelse condition is true)
-          # function "yr<-yr-ifelse(mn<wyMonth,1L,0L)" for mn>=2 == wyMonth=2:
-          # yr <- yr - 0 (ifelse condition is false)
-          # The first of February of a time series initiates a new
-          # water year and if the time series contains a leap year,
-          # the year for February must be kept as the current year
-          # (with the original function yr<-yr+ifelse(mn<wyMonth,0L,1L), 
-          #  February 2000 would be assigned water year 2001)
-          yr <- yr - ifelse(mn < wyMonth, 1L, 0L)
-        }else{
-          # If the water year starts in March (or later):
-          # yr for Jan and Feb must be the current year (as it can be a
-          # leap year)
-          # function "yr<-yr+ifelse(mn<wyMonth,0L,1L)":
-          # yr <- yr + 0
-          yr <- yr + ifelse(mn < wyMonth, 0L, 1L) 
-        }
-        if(numeric)
-                return(yr)
-        ordered(yr)
+  ## Coding history:
+  ##    2005Jul14 DLLorenz Initial dated verion
+  ##    2010Feb17 DLLorenz Added option to return numerics
+  ##    2011Jun07 DLLorenz Conversion to R
+  ##    2012Aug11 DLLorenz Integer fixes
+  ##    2013Feb15 DLLorenz Prep for gitHub
+  ##    2016Aug12 Joe Mills copied this from smwrBase
+  ##    2023Aug04 Jens Kiesel added different calculation if wyMonth < 7
+  ##              (see explanation below)
+  x <- as.POSIXlt(x)
+  yr <- x$year + 1900L
+  mn <- x$mon + 1L  # x$mon starts at 0 for Jan!
+  if (wyMonth < 7){
+    # it is most intuitive that the water year value should be the calendar 
+    # year with the most dates in the water year. Jan.-Jun. would be assigned
+    # the previous year as the water year value and Jul.-Dec. would be assigned
+    # the current year as the water year value.
+    
+    # If the water year is supposed to start in Jan-Jun (e.g. April):
+    # yr for Jan-Mar must be the previous year, 
+    # yr for Apr-Dec must be the current year 
+    yr <- yr - ifelse(mn < wyMonth, 1L, 0L)
+  }else{
+    # If the water year is supposed to start in Jul-Dec (e.g. September):
+    # yr for Jan-August must be the current year, 
+    # yr for Sep-Dec must be the next year 
+    yr <- yr + ifelse(mn < wyMonth, 0L, 1L) 
+  }
+  if(numeric)
+    return(yr)
+  ordered(yr)
 }
