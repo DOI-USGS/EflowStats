1f041371578afca36afca97774df81580bdfae49
jenskiesel <jenskiesel@gmail.com>
2023-09-22 05:29:54 +0200

    Needed to change test functions to "expect_equal_to_reference" as the target test files are not created with "expect_equal"; readRDS functions do not work when .rds files don't exist.

--- tests/testthat/tests_calc_durationLow.R
+++ tests/testthat/tests_calc_durationLow.R
@@ -5,14 +5,14 @@ test_that("duration low pref mean", {
         x<-sampleData[c("date","discharge")]
         
         calc_durationLowTest <- calc_durationLow(x=x,yearType="water",pref = "mean")
-        calc_durationLowTestCheck <- readRDS("data/tests_calc_durationLow.rds")
+        #calc_durationLowTestCheck <- readRDS("data/tests_calc_durationLow.rds")
         
-        expect_equal(calc_durationLowTest,calc_durationLowTestCheck)
+        expect_equal_to_reference(calc_durationLowTest,"data/tests_calc_durationLow.rds")
         
         x <- readRDS("data/sample_nwis_data.rds")
         calc_durationLowTest <- calc_durationLow(x=x,yearType="water",pref = "mean")
-        calc_durationLowTestCheck <- readRDS("data/tests_calc_durationLow_nwis_mean.rds")
-        expect_equal(calc_durationLowTest,calc_durationLowTestCheck)
+        #calc_durationLowTestCheck <- readRDS("data/tests_calc_durationLow_nwis_mean.rds")
+        expect_equal_to_reference(calc_durationLowTest,"data/tests_calc_durationLow_nwis_mean.rds")
         
 })
 
@@ -21,12 +21,11 @@ test_that("duration low pref median", {
         x<-sampleData[c("date","discharge")]
         
         calc_durationLowTest <- calc_durationLow(x=x,yearType="water", pref = "median")
-        calc_durationLowTestCheck <- readRDS("data/tests_calc_durationLow.rds")
-        
-        expect_equal(calc_durationLowTest,calc_durationLowTestCheck)
+        #calc_durationLowTestCheck <- readRDS("data/tests_calc_durationLow.rds")
+        expect_equal_to_reference(calc_durationLowTest,"data/tests_calc_durationLow.rds")
         
         x <- readRDS("data/sample_nwis_data.rds")
         calc_durationLowTest <- calc_durationLow(x=x,yearType="water", pref = "median")
-        calc_durationLowTestCheck <- readRDS("data/tests_calc_durationLow_nwis_median.rds")
-        expect_equal(calc_durationLowTest,calc_durationLowTestCheck)
+        #calc_durationLowTestCheck <- readRDS("data/tests_calc_durationLow_nwis_median.rds")
+        expect_equal_to_reference(calc_durationLowTest,"data/tests_calc_durationLow_nwis_median.rds")
 })
